0c258ffc6e031f08a5a25e604046ee4a2c10727d
salin-pant9 <scorpiopant@gmail.com>
2024-05-22 15:12:27 +0545

    navbar and background change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 // import './app.css';
-import ReactDom from 'react-dom/client';
-import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import ReactDom from "react-dom/client";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-import Navigation from './components/Navbar';
+import Navigation from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
-    < Navigation />
+      <Navigation />
+      <Hero />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-[85vh]"></div>
+  );
+};
+
+export default Hero;
