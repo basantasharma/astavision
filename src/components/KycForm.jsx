bbdc8aff1ca295ae21e04d48e69a255a01ae169e
basantasharma <gyawalibasanta0@gmail.com>
2024-05-22 17:03:49 +0545

    form is being added

---
+++ src/components/KycForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const KycForm = () => {
+  return (
+    <>
+    <div className='flex justify-center align-middle items-center'>KycForm</div>
+    
+    </>
+  )
+}
+
+export default KycForm;
